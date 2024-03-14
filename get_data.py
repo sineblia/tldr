fed803ce9f6c3b3a90d676361f3d5615146c145b
simoneb1x <moneb1x@gmail.com>
2024-03-14 13:26:01 +0100

    improved data collection script. now it's faster

--- get_data.py
+++ get_data.py
@@ -1,5 +1,7 @@
 import os
-import subprocess
+import boto3
+from botocore import UNSIGNED
+from botocore.client import Config
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
@@ -13,55 +15,59 @@ db_name = os.environ['DB_NAME']
 collection_name = os.environ['COLLECTION_NAME']
 
 client = MongoClient(mongo_conn_string, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged. Successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
 db = client[db_name]
 collection = db[collection_name]
 
-# S3 bucket details
-BUCKET = "s3://pmc-oa-opendata/oa_comm/xml/all/"
-MAX_FILES = 100000  # Max number of files to process
+# Setup AWS S3 client
+s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+bucket_name = 'pmc-oa-opendata'
+prefix = 'oa_comm/xml/all/'
 
-# Function to process files
+# Set max number of files and batch size for insertion
+MAX_FILES = 100000
+BATCH_SIZE = 100
+
 def process_files():
-    marker = ''  # Start without a marker
-    files_processed = 0
+    processed_files = 0
+    batch_data = []
+    
+    # Create an index on the 'filename' field if it doesn't exist for faster lookups
+    collection.create_index('filename', unique=True)
+
+    paginator = s3.get_paginator('list_objects_v2')
+    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
 
     with tqdm(total=MAX_FILES, desc="Processing files") as pbar:
-        while files_processed < MAX_FILES:
-            command = f"aws s3 ls {BUCKET} --no-sign-request {marker}"
-            proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        for page in page_iterator:
+            if processed_files >= MAX_FILES:
+                break
+            for obj in page['Contents']:
+                key = obj['Key']
+                # Check if the file is already in the database
+                if collection.count_documents({'filename': key}) == 0:
+                    response = s3.get_object(Bucket=bucket_name, Key=key)
+                    xml_content = response['Body'].read()
 
-            while True:
-                line = proc.stdout.readline()
-                if not line or files_processed >= MAX_FILES:
-                    break
-                file_list = line.decode('utf-8').strip()
-                FILENAME = file_list.split()[-1]
+                    # Append to batch data
+                    batch_data.append({'filename': key, 'content': xml_content})
+                    processed_files += 1
 
-                # Use aws s3 cp to copy the file content to stdout and capture it
-                copy_command = f"aws s3 cp {BUCKET}{FILENAME} - --no-sign-request"
-                file_proc = subprocess.run(copy_command, shell=True, stdout=subprocess.PIPE, text=True)
+                    # If batch size is reached, insert into MongoDB
+                    if len(batch_data) == BATCH_SIZE:
+                        collection.insert_many(batch_data)
+                        batch_data = []  # Reset the batch data list
 
-                xml_content = file_proc.stdout
+                    pbar.update(1)
 
-                # Insert the XML content into MongoDB
-                collection.insert_one({'filename': FILENAME, 'content': xml_content})
+                    # Break if max files reached
+                    if processed_files >= MAX_FILES:
+                        break
 
-                files_processed += 1
-                pbar.update(1)  # Update progress bar
+            # Insert any remaining files in batch_data
+            if batch_data:
+                collection.insert_many(batch_data)
 
-                if files_processed >= MAX_FILES:
-                    break
-
-            # Set the last file processed as the marker for the next command
-            marker = f"--start-after {FILENAME}"
+    print(f"Data collection complete. {processed_files} new files added.")
 
 process_files()
-print("Upload completed.")
+print("Data collection complete.")
